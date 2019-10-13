bea6752ea4a7de321104709956520f65ac515d33
seungdols <seungdols0822@gmail.com>
2019-10-13 21:32:18 +0900

    Add solution of 326.power of three

---
+++ algorithm/326.power-of-three.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=326 lang=javascript
+ *
+ * [326] Power of Three
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+  return findPower(n); 
+};
+
+function findPower (num) {
+  if (num === 0) return false;
+  if (num === 1) return true;
+  return findPower(num / 3);
+};
+// @lc code=end
+
