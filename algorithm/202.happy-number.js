6e7552ceeedb63d2eb8ad9455314e152344e7f75
seungdols <seungdols0822@gmail.com>
2020-08-23 22:04:56 +0900

    Add 202. Happy Number

---
+++ algorithm/202.happy-number.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=202 lang=javascript
+ *
+ * [202] Happy Number
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+  let result = n
+  let memo = []
+    while (result !== 1) {
+      memo.push(result)
+      const value = JSON.stringify(result).split('')
+                    .map(v => Math.pow(+v, 2))
+                    .reduce((pre, cur) => (pre + cur), 0)
+      result = value
+      if (memo.includes(result)) {
+        return false
+      }
+    }
+  return true
+};
+// @lc code=end
+
