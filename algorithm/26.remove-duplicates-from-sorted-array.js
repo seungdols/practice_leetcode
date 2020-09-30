c97f19b511d054819e05660da856cda4c4e01698
seungdols <seungdols0822@gmail.com>
2020-10-01 00:33:14 +0900

    Add 26. Remove Duplicates from Sorted Array

---
+++ algorithm/26.remove-duplicates-from-sorted-array.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=26 lang=javascript
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  for (let i = 0; i < nums.length; i = i + 1) {
+    for (let j = i + 1; j < nums.length; j = j + 1) {
+      if (nums[i] === nums[j]) {
+        console.log(nums[j], j)
+        nums.splice(j, 1)
+        j--
+      } 
+    }
+  }
+  return nums.length
+};
+// @lc code=end
+
