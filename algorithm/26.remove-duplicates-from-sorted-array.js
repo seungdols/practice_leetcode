d7638d7e8bb3490869513ca4cc5f40fc3527eaea
seungdols <seungdols0822@gmail.com>
2020-10-01 00:37:38 +0900

    Delete console.log

--- algorithm/26.remove-duplicates-from-sorted-array.js
+++ algorithm/26.remove-duplicates-from-sorted-array.js
@@ -13,7 +13,6 @@ var removeDuplicates = function(nums) {
   for (let i = 0; i < nums.length; i = i + 1) {
     for (let j = i + 1; j < nums.length; j = j + 1) {
       if (nums[i] === nums[j]) {
-        console.log(nums[j], j)
         nums.splice(j, 1)
         j--
       } 
