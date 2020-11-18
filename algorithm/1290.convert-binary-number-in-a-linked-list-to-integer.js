609bd3c2bffc99ca68b5830a594883a0fddec06f
seungdols <seungdols0822@gmail.com>
2020-11-18 23:56:21 +0900

    Add 1290 solution

---
+++ algorithm/1290.convert-binary-number-in-a-linked-list-to-integer.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=1290 lang=javascript
+ *
+ * [1290] Convert Binary Number in a Linked List to Integer
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function (head) {
+  let node = head
+  const arr = []
+  while (node !== null) {
+    arr.push(node.val)
+    node = node.next
+  }
+
+  return parseInt(arr.join(""), 2)
+}
+// @lc code=end
