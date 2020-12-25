983cedda44f34fbcfc8feee31b22cfc7e7bbb18b
seungdols <seungdols0822@gmail.com>
2020-12-25 22:14:30 +0900

    Update answer of 1512 number of good pairs

--- algorithm/1512.number-of-good-pairs.js
+++ algorithm/1512.number-of-good-pairs.js
@@ -9,16 +9,32 @@
  * @param {number[]} nums
  * @return {number}
  */
-var numIdenticalPairs = function(nums) {
-  let count = 0;
+var numIdenticalPairs = function (nums) {
+  // let count = 0;
+  // for (let i = 0; i < nums.length; i++) {
+  //     for (let j = i + 1; j < nums.length; j++) {
+  //         if (nums[i] === nums[j] && i < j) {
+  //             count = count + 1
+  //         }
+  //     }
+  // }
+  // return count
+  let map = new Map()
+
   for (let i = 0; i < nums.length; i++) {
-      for (let j = i + 1; j < nums.length; j++) {
-          if (nums[i] === nums[j] && i < j) {
-              count = count + 1
-          }
-      }
+    if (!map.has(nums[i])) {
+      map.set(nums[i], 0)
+    }
+    map.set(nums[i], map.get(nums[i]) + 1)
+  }
+
+  let count = 0;
+  for (const key of map.keys()) {
+    if (1 < map.get(key)) {
+      const n = map.get(key)
+      count += Math.floor((n * (n - 1) / 2))
+    }
   }
   return count
 };
 // @lc code=end
-
