b46b81cd7f871c3cd3d4ddb98d108d19ecd1ef4d
seungdols <seungdols0822@gmail.com>
2020-12-25 21:09:12 +0900

    Add solution of 1512 number of good pairs

---
+++ algorithm/1512.number-of-good-pairs.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1512 lang=javascript
+ *
+ * [1512] Number of Good Pairs
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+          if (nums[i] === nums[j] && i < j) {
+              count = count + 1
+          }
+      }
+  }
+  return count
+};
+// @lc code=end
+
