0fa8d800d37fbb6d291c3fd3cfb8f23c3651ed5c
seungdols <seungdols0822@gmail.com>
2021-02-01 20:23:10 +0900

    Add solution of 344nd problem

---
+++ algorithm/344.reverse-string.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=344 lang=javascript
+ *
+ * [344] Reverse String
+ */
+
+// @lc code=start
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+  return s.reverse()
+};
+// @lc code=end
+
