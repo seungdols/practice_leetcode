d65cb342e8eeaa3e63424262161086abb784a694
seungdols <seungdols0822@gmail.com>
2021-03-17 22:58:01 +0900

    Update solution of 217nd problem

--- algorithm/217.contains-duplicate.js
+++ algorithm/217.contains-duplicate.js
@@ -10,22 +10,44 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-  const result = []
+  // TODO: 첫번째 
+  // const result = []
+  
+  // for (const num of nums) {
+  //   result[num] = result[num] ? result[num] + 1 : 1
+  // }
+  
+  // let check = false
+  // for (let i = 0; i < result.length; i++) {
+  //   result[i] = result[i] ? result[i] : 0
+  //   if (result[i] > 1) {
+  //     check = true
+  //     break
+  //   }
+  // }
+  
+  // return check
+
+  // TODO: 두번째 - loop 하나를 줄임. (쓸데없이 undefined를 지우려고 함.)
+  // const result = []
+  
+  // for (const num of nums) {
+  //   result[num] = result[num] ? result[num] + 1 : 1
+  //   if (result[num] > 1) return true
+  // }
+  
+  // return false
+
+  //TODO: 세번째 Set 사용 
+  const result = new Set()
   
   for (const num of nums) {
-    result[num] = result[num] ? result[num] + 1 : 1
+     if (result.has(num)) {
+         return true
+     }
+     result.add(num)
   }
-  
-  let check = false
-  for (let i = 0; i < result.length; i++) {
-    result[i] = result[i] ? result[i] : 0
-    if (result[i] > 1) {
-      check = true
-      break
-    }
-  }
-  
-  return check
+  return false
 };
 // @lc code=end
 
