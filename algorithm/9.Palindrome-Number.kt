5383219f606126a896c78b376a9c439d05f58124
seungdols <seungdols0822@gmail.com>
2022-03-15 22:04:34 +0900

    Add solve of 9. Palindrome Number

---
+++ algorithm/9.Palindrome-Number.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun isPalindrome(x: Int): Boolean {
+      if (x < 0 || (x % 10 == 0 && x != 0))  return false
+       
+      var origin = x  
+      var reverted = 0
+      while(origin > reverted) {
+          reverted = reverted * 10 + origin % 10
+          println(reverted)
+          origin = origin / 10
+      }
+      
+      return origin == reverted || origin == reverted / 10
+    }
+}
