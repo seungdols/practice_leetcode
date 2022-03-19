2fe3e2cd1b80d7e68a56bad29077563eb56eab40
seungdols <seungdols0822@gmail.com>
2022-03-20 02:20:36 +0900

    Create 142.Linked-List-Cycle-II.kt

---
+++ algorithm/142.Linked-List-Cycle-II.kt
@@ -0,0 +1,31 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun detectCycle(head: ListNode?): ListNode? {
+       
+       var first = head
+       var second = head
+       var cyclePos = head  
+       while(first != null && first.next != null) {  
+           first = first?.next?.next
+           second = second?.next
+           if (first == second) {
+               while(second != cyclePos) {
+                    second = second?.next
+                    cyclePos = cyclePos?.next
+               }
+               return cyclePos
+           }
+       }
+       
+       return null
+    }
+}
